78b6be0a07f1d4ae05ab2332b1a018bd84beb072
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-12-02 19:32:57 +0600

    update paht

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import Layouts from './layouts/Layouts';
-import ErrorPage from './components/shared/pages/error-page/ErrorPage ';
-import WrapperMainPage from './components/shared/pages/mainPageMain/WrapperMainPage';
+import ErrorPage from './components/pages/error-page/ErrorPage ';
+import WrapperMainPage from './components/pages/mainPageMain/WrapperMainPage';
 
 const App = () => {
   const router = createBrowserRouter([
